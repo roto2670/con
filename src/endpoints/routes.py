824a0c0f666d7fcac4b13d40ee6b6811d008d3d8
Jslee <jslee@narantech.com>
2018-12-26 18:59:22 +0900

    Miss fix

--- src/endpoints/routes.py
+++ src/endpoints/routes.py
@@ -264,7 +264,7 @@ def get_logs(product_id):
     return render_template('logs.html', logs=logs, keyword=_keyword,
                            token=token, limit=_limit)
   else:
-    rets = apis.get_logs(product_id, keyword=_keyword)
+    rets = apis.get_logs(product_id, keyword=_keyword, limit=_limit)
     token = rets.get('token', None)
     logs = rets.get('logs', [])
     return render_template('logs.html', logs=logs, token=token,
