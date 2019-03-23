e5b93e0b51e3e4ffd5524717632bf8179fe97361
Jslee <jslee@narantech.com>
2019-03-23 22:35:36 +0900

    Fixed namecloud

--- src/home/routes.py
+++ src/home/routes.py
@@ -89,10 +89,11 @@ def _build_product_info(product_id):
           _tmp_model_dict[gadget['model_name']] = 1
       _tmp_battery += gadget.get('battery', 0)
       __name = gadget['name'].replace("'", "`").lower()
-      if __name in _tmp_name_dict:
-        _tmp_name_dict[__name] += 1
-      else:
-        _tmp_name_dict[__name] = 1
+      if product_id not in __name:
+        if __name in _tmp_name_dict:
+          _tmp_name_dict[__name] += 1
+        else:
+          _tmp_name_dict[__name] = 1
 
       if 'locale' in gadget and gadget['locale']:
         _locale = gadget['locale'].lower()
