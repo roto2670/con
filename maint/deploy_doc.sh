295203ad361dafde82d045fc34629f5e2a0e8c95
lhtak <lhtak@narantech.com>
2019-05-21 14:59:40 +0900

    Changed doc deploy path

--- maint/deploy_doc.sh
+++ maint/deploy_doc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-SERVER_IP=192.168.1.82
+SERVER_IP=192.168.3.192
 DEVSRC=~/desktop/doc_output/console
-scp -r $DEVSRC/* root@$SERVER_IP:/apps/nginx_cide/home/nginx_cide/www/mibiot-console/
+scp -r $DEVSRC/* root@$SERVER_IP:/www/mibiot-console/
