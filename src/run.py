ea64fd34057a3303accf4573cca6e3fc25028772
Jslee <jslee@narantech.com>
2019-05-27 14:17:54 +0900

    Comment out dashboard library

--- src/run.py
+++ src/run.py
@@ -18,7 +18,7 @@ from importlib import import_module
 
 from flask import Flask  # noqa : pylint: disable=import-error
 from flask_cors import CORS
-import flask_monitoringdashboard as dashboard
+#import flask_monitoringdashboard as dashboard
 
 import apis
 import dash_apis
@@ -109,17 +109,17 @@ if not apis.IS_DEV:
   # gunicorn
   __app = create_app()
   __app.debug = False
-  dashboard.config.init_from(file="./config.cfg")
-  dashboard.config.database_name = __app.config.get('SQLALCHEMY_DATABASE_URI')
-  dashboard.bind(__app)
+  #dashboard.config.init_from(file="./config.cfg")
+  #dashboard.config.database_name = __app.config.get('SQLALCHEMY_DATABASE_URI')
+  #dashboard.bind(__app)
 
 
 if  __name__ == '__main__':
   _app = create_app()
   if apis.IS_DEV:
     _app.debug = True
-    dashboard.config.init_from(file="./config.cfg")
-    dashboard.bind(_app)
+    #dashboard.config.init_from(file="./config.cfg")
+    #dashboard.bind(_app)
     CORS(_app)
     _app.run(host='127.0.0.1', port=5000)
   else:
@@ -128,7 +128,7 @@ if  __name__ == '__main__':
     ssl_crt = os.path.join(ssl_path, 'mib_io.crt')
     ssl_key = os.path.join(ssl_path, 'mib_io.key')
     _app.debug = False
-    dashboard.config.init_from(file="./config.cfg")
-    dashboard.config.database_name = _app.config.get('SQLALCHEMY_DATABASE_URI')
-    dashboard.bind(_app)
+    #dashboard.config.init_from(file="./config.cfg")
+    #dashboard.config.database_name = _app.config.get('SQLALCHEMY_DATABASE_URI')
+    #dashboard.bind(_app)
     _app.run(host='127.0.0.1', port=5000, ssl_context=(ssl_crt, ssl_key))
