80c9cb4801e84ad738095a1da0bf62efe15b4aa8
Nammyeong Kim <nmkim@narantech.com>
2020-08-22 19:35:26 +0900

    Add firebase admin module in server
    * Add header uis in users menu.
    * Add quries to firestore for stats and users.

--- src/covid19/routes.py
+++ src/covid19/routes.py
@@ -15,6 +15,9 @@ import logging
 
 from flask import render_template, redirect, request  # noqa : pylint: disable=import-error
 from flask_login import current_user  # noqa : pylint: disable=import-error
+import firebase_admin as fbase
+from firebase_admin import firestore
+from firebase_admin import credentials
 
 import base
 import util
@@ -23,15 +26,63 @@ import in_apis
 from covid19 import blueprint
 
 
+# Initialize firebase
+cred = credentials.Certificate("../res/fbase_key/service.json")
+fbase_app = fbase.initialize_app(cred)
+
+stats = {} # today's stats
+
 @blueprint.route('/')
 @util.require_login
 def route_default():
-  return render_template("dashboard.html")
+  # Gets today stats from firebase store.
+  # TODO: update daily
+  fbase_store = firestore.client()
+  global stats
+  if not stats:
+    # query to firebase.
+    stat_ref = fbase_store.collection(u'covid19_stats')
+    query = stat_ref.order_by(u'ts', direction=firestore.Query.DESCENDING).limit(1)
+    results = query.stream()
+    try:
+      doc = next(results)
+      stats = doc.to_dict()
+    except StopIteration:
+      pass
+
+  return render_template("dashboard.html", stats=stats)
+
+@blueprint.route('/stats', methods=['GET'])
+@util.require_login
+def default_today_stats():
+  fbase_store = firestore.client()
+  stat_ref = fbase_store.collection(u'covid19_stats')
+  query = stat_ref.order_by(u'ts', direction=firestore.Query.DESCENDING).limit(1)
+  results = query.stream()
+  stats = {}
+  try:
+    doc = next(results)
+    stats = doc.to_dict()
+  except StopIteration:
+    pass
+
+  return json.dumps(stats, indent=4, sort_keys=True, default=str)
 
 @blueprint.route('/users')
 @util.require_login
 def route_users():
   return render_template("users.html")
+
+@blueprint.route('/users', methods=['GET'])
+@util.require_login
+def default_users():
+  # Get users from firebase firestore
+  docs = fbase_store.collection('users').stream()
+  users = []
+  for doc in docs:
+    users.append(doc.to_dict())
+
+  return json.dumps(users, indent=4, sort_keys=True, default=str)
 
 @blueprint.route('/notifications')
 @util.require_login
